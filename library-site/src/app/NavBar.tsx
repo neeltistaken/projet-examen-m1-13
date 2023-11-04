d44c8c9e7801d8d9d33f678bbc70163713bae1ef
Guillaume Leroy <Guillaume.Leroy0208@gmail.com>
2023-11-04 20:02:27 +0100

    page auteurs bouton modifier et creer à finir

--- library-site/src/app/NavBar.tsx
+++ library-site/src/app/NavBar.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
 
-function Navbar({ current_page }: {current_page: string}) {
-  let styleSelected: string = "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium";
-  let styleUnselected: string = "text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium";
+function Navbar({ currentPage }: { currentPage: string }) {
+  const navLinks = [
+    { text: 'Accueil', path: '/' },
+    { text: 'Livres', path: '/books' },
+    { text: 'Auteurs', path: '/authors' },
+    { text: 'Utilisateurs', path: '/users' },
+  ];
+
+  const linkStyle = (linkText: string) => {
+    return currentPage === linkText
+      ? 'bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium'
+      : 'text-gray-300 hover-bg-gray-700 hover-text-white rounded-md px-3 py-2 text-sm font-medium';
+  };
+
   return (
     <nav className="bg-gray-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+            <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover-bg-gray-700 hover-text-white focus-outline-none focus-ring-2 focus-ring-inset focus-ring-white" aria-controls="mobile-menu" aria-expanded="false">
               <span className="absolute -inset-0.5"></span>
               <span className="sr-only">Open main menu</span>
-              <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+              <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
               </svg>
-              <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
               </svg>
             </button>
           </div>
@@ -27,17 +38,18 @@ function Navbar({ current_page }: {current_page: string}) {
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                <a href="/" className={current_page === "Accueil" ? styleSelected : styleUnselected} aria-current="page">Accueil</a>
-                <a href="/books" className={current_page === "Livres" ? styleSelected : styleUnselected}>Livres</a>
-                <a href="/authors" className={current_page === "Auteurs" ? styleSelected : styleUnselected}>Auteurs</a>
-                <a href="/users" className={current_page === "Utilisateurs" ? styleSelected : styleUnselected}>Utilisateurs</a>
+                {navLinks.map((link) => (
+                  <a key={link.text} href={link.path} className={linkStyle(link.text)} aria-current="page">
+                    {link.text}
+                  </a>
+                ))}
               </div>
             </div>
           </div>
         </div>
       </div>
     </nav>
-);
+  );
 }
 
 export default Navbar;
--- library-site/src/app/authors/page.tsx
+++ library-site/src/app/authors/page.tsx
@@ -1,28 +1,88 @@
 'use client';
 import React, { FC, useEffect, useState } from 'react';
-import axios from 'axios';
-import { PlainAuthorModel } from '../../models/author.model';
-import Navbar from "@/app/NavBar";
+import axios, { AxiosError } from 'axios';
+import { PlainAuthorPresenter } from '../../../../library-api/src/controllers/authors/author.presenter';
+import Navbar from '@/app/NavBar';
+import Modal from 'react-modal';
+
+type CreateAuthorData = {
+  firstName: string;
+  lastName: string;
+  photoUrl: string;
+};
 
 const AuthorsPage: FC = () => {
-  const [authors, setAuthors] = useState<PlainAuthorModel[]>([]);
+  const [authors, setAuthors] = useState<PlainAuthorPresenter[]>([]);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<string | null>(null);
+
+  // State pour la fenêtre modale
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [newAuthorData, setNewAuthorData] = useState<CreateAuthorData>({
+    firstName: '',
+    lastName: '',
+    photoUrl: '',
+  });
 
   useEffect(() => {
-    setLoading(true);
+    const loadAuthors = async () => {
+      setLoading(true);
 
-    axios
-      .get('http://localhost:3001/authors')
-      .then((response) => {
+      try {
+        const response = await axios.get('http://localhost:3001/authors');
         setAuthors(response.data);
         setLoading(false);
-      })
-      .catch((e) => {
-        setError(e);
+      } catch (e) {
+        setError((e as AxiosError).message);
         setLoading(false);
-      });
+      }
+    };
+
+    loadAuthors();
   }, []);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const createAuthor = async () => {
+    console.log('Création d\'un nouvel auteur');
+    try {
+      const response = await axios.post('http://localhost:3001/authors', newAuthorData);
+      console.log('Données renvoyées par l\'API :', response.data);
+
+      setAuthors([...authors, response.data]);
+      closeModal();
+    } catch (e) {
+      setError((e as AxiosError).message);
+    }
+  };
+
+  const updateAuthor = async (authorId: string, updatedAuthorData: CreateAuthorData) => {
+    try {
+      const response = await axios.put(`http://localhost:3001/authors/${authorId}`, updatedAuthorData);
+      const updatedAuthors = authors.map((author) =>
+        author.id === authorId ? response.data : author
+      );
+      setAuthors(updatedAuthors);
+    } catch (e) {
+      setError((e as AxiosError).message);
+    }
+  };
+
+  const deleteAuthor = async (authorId: string) => {
+    try {
+      await axios.delete(`http://localhost:3001/authors/${authorId}`);
+      const updatedAuthors = authors.filter((author) => author.id !== authorId);
+      setAuthors(updatedAuthors);
+    } catch (e) {
+      setError((e as AxiosError).message);
+    }
+  };
 
   return (
     <div>
@@ -33,21 +93,103 @@ const AuthorsPage: FC = () => {
         {loading && <p className="text-gray-600 text-center">Chargement...</p>}
         {error && <p className="text-red-600 text-center">{`Erreur : ${error}`}</p>}
 
+        <button
+          onClick={openModal}
+          className="bg-blue-500 text-white font-semibold px-4 py-2 rounded-full hover:bg-blue-700 mr-2"
+        >
+          Créer un auteur
+        </button>
+
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-4">
           {authors.map((author) => (
             <li key={author.id} className="bg-white shadow-lg rounded-lg">
               <img
-                src={`https://${author.photoUrl}`} 
+                src={`https://${author.photoUrl}`}
                 alt={`${author.firstName} ${author.lastName}`}
                 className="w-full h-64 object-cover rounded-t-lg"
               />
               <div className="p-4">
                 <h2 className="text-xl font-semibold text-center">{`${author.firstName} ${author.lastName}`}</h2>
+                <button
+                  onClick={() => {
+                    // Ouvrir la fenêtre modale de mise à jour ici
+                  }}
+                  className="bg-blue-500 text-white font-semibold px-4 py-2 rounded-full hover-bg-blue-700 mr-2"
+                >
+                  Modifier
+                </button>
+                <button
+                  onClick={() => deleteAuthor(author.id)}
+                  className="bg-red-500 text-white font-semibold px-4 py-2 rounded-full hover-bg-red-700"
+                >
+                  Supprimer
+                </button>
               </div>
             </li>
           ))}
         </ul>
       </div>
+
+      {/* Fenêtre modale pour la création d'auteur */}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Créer un auteur"
+        appElement={document.getElementById('root') || undefined}
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 border rounded-md max-w-md w-full"
+      >
+        <h2 className="text-2xl font-bold mb-4">Créer un nouvel auteur</h2>
+        <form>
+          <div className="mb-4">
+            <label className="block font-semibold">Prénom:</label>
+            <input
+              type="text"
+              value={newAuthorData.firstName}
+              onChange={(e) =>
+                setNewAuthorData({ ...newAuthorData, firstName: e.target.value })
+              }
+              className="w-full p-2 border rounded-md"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block font-semibold">Nom:</label>
+            <input
+              type="text"
+              value={newAuthorData.lastName}
+              onChange={(e) =>
+                setNewAuthorData({ ...newAuthorData, lastName: e.target.value })
+              }
+              className="w-full p-2 border rounded-md"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block font-semibold">URL de la photo:</label>
+            <input
+              type="text"
+              value={newAuthorData.photoUrl}
+              onChange={(e) =>
+                setNewAuthorData({ ...newAuthorData, photoUrl: e.target.value })
+              }
+              className="w-full p-2 border rounded-md"
+            />
+          </div>
+          <button
+            type="button"
+            onClick={createAuthor}
+            className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-full hover:bg-blue-700 mr-2"
+          >
+            Créer
+          </button>
+          <button
+            type="button"
+            onClick={closeModal}
+            className="bg-gray-300 text-gray-600 font-semibold py-2 px-4 rounded-full hover:bg-gray-400"
+          >
+            Annuler
+          </button>
+        </form>
+      </Modal>
+
     </div>
   );
 };
