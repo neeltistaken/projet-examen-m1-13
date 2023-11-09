05bae16d028c170c15e457d65fe18ba586cdb81e
Simon de LAJARTRE <72396434+sljtre@users.noreply.github.com>
2023-11-09 10:03:34 +0100

    feat(home page): add logo on home page

--- library-site/src/app/nav-bar.tsx
+++ library-site/src/app/nav-bar.tsx
@@ -17,14 +17,16 @@ export function NavBar() {
   return (
     <nav className="bg-gray-800">
       <div className="mx-auto px-2 sm:px-6 py-2 sm:py-4 flex items-center justify-center gap-4">
-        <Image src="/logo.png" alt="Logo" width={50} height={50} />
+        <Link href="/">
+          <Image src="/logo.png" alt="Logo" width={50} height={50} />
+        </Link>
         <div className="flex flex-wrap sm:gap-4 flex-col sm:flex-row">
           {navLinks.map((link) => (
             <Link
               key={link.text}
               href={link.path}
-              className={`text-gray-300 px-3 py-1 rounded-md hover:bg-gray-900 ${
-                pathname === link.path ? 'text-primary-500 font-semibold' : ''
+              className={`text-gray-300 px-3 py-1 rounded-md font-semibold hover:bg-gray-900 ${
+                pathname === link.path && 'text-primary-500'
               }`}
             >
               {link.text}
--- library-site/src/app/page.tsx
+++ library-site/src/app/page.tsx
@@ -1,8 +1,12 @@
 import { FC, ReactElement } from 'react';
+import Image from 'next/image';
 
 const Home: FC = (): ReactElement => (
-  <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    Biblio'tech
+  <main className="flex flex-col items-center pt-24">
+    <Image src="/logo.png" alt="logo" width={200} height={200} />
+    <h1 className="text-6xl font-bold text-center text-gray-800">
+      Biblio'tech
+    </h1>
   </main>
 );
 
