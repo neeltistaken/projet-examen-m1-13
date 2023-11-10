f7f8baf767602a1238bce179a8efd73fc6a30a50
Simon de LAJARTRE <72396434+sljtre@users.noreply.github.com>
2023-11-10 08:45:26 +0100

    feat(authors/[id]): show artiste information

--- library-site/src/app/authors/[id]/page.tsx
+++ library-site/src/app/authors/[id]/page.tsx
@@ -13,18 +13,36 @@ const AuthorDetailsPage: FC = () => {
   if (error) {
     return <p>{`Erreur : ${error.message}`}</p>;
   } else if (!author) {
-    return <p>Chargement...</p>;
+    return (
+      <h1 className="text-3xl font-bold mb-4 mt-10 text-center">
+        Chargement...
+      </h1>
+    );
   } else
     return (
       <>
-        <h1 className="text-3xl font-bold mb-4 mt-10 text-center">
+        <h1 className="text-3xl font-bold mb-8 mt-10 text-center">
           {author.firstName} {author.lastName}
         </h1>
-        <img
-          src={`https://${author.photoUrl}`}
-          alt={`Photo de ${author.firstName} ${author.lastName}`}
-          className="w-72 h-72 rounded-full object-cover"
-        />
+        <div className="flex justify-center flex-wrap xs:gap-10">
+          <img
+            src={`https://${author.photoUrl}`}
+            alt={`Photo de ${author.firstName} ${author.lastName}`}
+            className="w-48 h-48 sm:w-72 sm:h-72 rounded-full object-cover"
+          />
+          <div>
+            <h2 className="text-xl font-bold mb-4 mt-5">Livres</h2>
+            {author.books?.length ? (
+              <ul className="list-disc list-inside">
+                {author.books.map((book) => (
+                  <li key={book.id}>{book.name}</li>
+                ))}
+              </ul>
+            ) : (
+              <p>Aucun livre n'a été trouvé pour cet auteur.</p>
+            )}
+          </div>
+        </div>
       </>
     );
 };
